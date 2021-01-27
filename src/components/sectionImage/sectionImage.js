2e47dac794c81140c88245077d89e8e5ef7a1aa4
NicolasBourret <61506584+NicolasBourret@users.noreply.github.com>
2021-01-27 11:55:58 +0100

    Update sectionImage.js

--- src/components/sectionImage/sectionImage.js
+++ src/components/sectionImage/sectionImage.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { HeroImage } from "../hero/hero"
-import SectionImageStyles from "./sectionimage.module.css"
+import SectionImageStyles from "./sectionImage.module.css"
 
 export default function sectionImage() {
   return (
